b6977aa8e0df11ed9f9b68ad97fe8bda74bee678
sebestin <69845469+sebestin@users.noreply.github.com>
2021-11-17 00:39:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+       slidetoshow:3,
+       slideToScroll:1,
+       prevArrow:".prev-btn",
+       nextArrow:".next-btn",
+
+    });
+});
